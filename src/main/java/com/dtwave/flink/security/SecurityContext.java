f10c0a0bbd4dbb2a379f206ac0900714828c014e
白松 <song.bs@dtwave-inc.com>
2022-12-13 23:18:00 +0800

    first commit

---
+++ src/main/java/com/dtwave/flink/security/SecurityContext.java
@@ -0,0 +1,123 @@
+package com.dtwave.flink.security;
+
+import com.google.common.collect.Table;
+
+import org.apache.calcite.sql.SqlBasicCall;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.TableResult;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.api.internal.TableEnvironmentImpl;
+import org.apache.flink.util.FlinkRuntimeException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+
+import static com.dtwave.flink.security.constant.Constant.SECURITY_USERNAME;
+
+/**
+ * @description: SecurityContext
+ * @author: baisong
+ * @version: 1.0.0
+ * @date: 2022/12/10 12:16 PM
+ */
+public class SecurityContext {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SecurityContext.class);
+
+    private static volatile SecurityContext singleton;
+
+    private final TableEnvironmentImpl tableEnv;
+
+    private Table<String, String, String> rowLevelPermissions;
+    private final Random rand = new Random();
+
+    public static SecurityContext getInstance() {
+        if (singleton == null) {
+            synchronized (SecurityContext.class) {
+                if (singleton == null) {
+                    singleton = new SecurityContext();
+                }
+            }
+        }
+        return singleton;
+    }
+
+
+    private SecurityContext() {
+        Configuration configuration = new Configuration();
+        int port = randInt(8082, 8188);
+        configuration.setInteger("rest.port", port);
+        LOG.info("WebUI is http://127.0.0.1:{}", port);
+        configuration.setBoolean("table.dynamic-table-options.enabled", true);
+
+        try (StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment(configuration)
+                .enableCheckpointing(3 * 1000L)
+                .setParallelism(1)) {
+            EnvironmentSettings settings = EnvironmentSettings.newInstance()
+                    .inStreamingMode()
+                    .build();
+
+            this.tableEnv = (TableEnvironmentImpl) StreamTableEnvironment.create(env, settings);
+        } catch (Exception e) {
+            LOG.error("init flink execution environment error: ", e);
+            throw new FlinkRuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Add row-level filter conditions and return new SQL
+     */
+    public String addRowLevelFilter(String username, String singleSql) {
+        System.setProperty(SECURITY_USERNAME, username);
+
+        // in the modified SqlSelect, filter conditions will be added to the where clause
+        SqlNode parsedTree = tableEnv.getParser().parseSql(singleSql);
+        return parsedTree.toString();
+    }
+
+
+    /**
+     * Query the configured permission point according to the user name and table name, and return
+     * it to SqlBasicCall
+     */
+    public SqlBasicCall queryPermissions(String username, String tableName) {
+        String permissions = rowLevelPermissions.get(username, tableName);
+        LOG.info("username: {}, tableName: {}, permissions: {}", username, tableName, permissions);
+        if (permissions != null) {
+            return (SqlBasicCall) tableEnv.getParser().parseExpression(permissions);
+        }
+        return null;
+    }
+
+
+    /**
+     * Execute the single sql
+     */
+    public TableResult execute(String singleSql) {
+        LOG.info("execute sql: {}", singleSql);
+        return tableEnv.executeSql(singleSql);
+    }
+
+
+    /**
+     * Execute the single sql with user permissions
+     */
+    public TableResult execute(String username, String singleSql) {
+        String newSql = addRowLevelFilter(username, singleSql);
+        LOG.info("new sql: {}", newSql);
+        return tableEnv.executeSql(newSql);
+    }
+
+    public void setRowLevelPermissions(Table<String, String, String> rowLevelPermissions) {
+        this.rowLevelPermissions = rowLevelPermissions;
+    }
+
+    private int randInt(int min, int max) {
+        return rand.nextInt((max - min) + 1) + min;
+    }
+}
---
+++ src/main/java/org/apache/calcite/sql/SqlSelect.java
@@ -0,0 +1,309 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.calcite.sql;
+
+import com.dtwave.flink.security.SecurityContext;
+
+import org.apache.calcite.sql.parser.SqlParserPos;
+import org.apache.calcite.sql.validate.SqlValidator;
+import org.apache.calcite.sql.validate.SqlValidatorScope;
+import org.apache.calcite.util.ImmutableNullableList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.annotation.Nonnull;
+
+import static com.dtwave.flink.security.constant.Constant.SECURITY_USERNAME;
+
+/**
+ * A <code>SqlSelect</code> is a node of a parse tree which represents a select statement. It
+ * warrants its own node type just because we have a lot of methods to put somewhere.
+ */
+public class SqlSelect extends SqlCall {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SqlSelect.class);
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    // constants representing operand positions
+    public static final int FROM_OPERAND = 2;
+    public static final int WHERE_OPERAND = 3;
+    public static final int HAVING_OPERAND = 5;
+
+    SqlNodeList keywordList;
+    SqlNodeList selectList;
+    SqlNode from;
+    SqlNode where;
+    SqlNodeList groupBy;
+    SqlNode having;
+    SqlNodeList windowDecls;
+    SqlNodeList orderBy;
+    SqlNode offset;
+    SqlNode fetch;
+    SqlNodeList hints;
+
+    //~ Constructors -----------------------------------------------------------
+
+    public SqlSelect(SqlParserPos pos,
+                     SqlNodeList keywordList,
+                     SqlNodeList selectList,
+                     SqlNode from,
+                     SqlNode where,
+                     SqlNodeList groupBy,
+                     SqlNode having,
+                     SqlNodeList windowDecls,
+                     SqlNodeList orderBy,
+                     SqlNode offset,
+                     SqlNode fetch,
+                     SqlNodeList hints) {
+        super(pos);
+        this.keywordList = Objects.requireNonNull(keywordList != null
+                ? keywordList : new SqlNodeList(pos));
+        this.selectList = selectList;
+        this.from = from;
+        this.groupBy = groupBy;
+        this.having = having;
+        this.windowDecls = Objects.requireNonNull(windowDecls != null
+                ? windowDecls : new SqlNodeList(pos));
+        this.orderBy = orderBy;
+        this.offset = offset;
+        this.fetch = fetch;
+        this.hints = hints;
+
+        // add condition for where clause
+        this.where = addCondition(from, where);
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    private SqlNode addCondition(SqlNode from, SqlNode where) {
+        LOG.info("from: {}, where: {}", from, where);
+        if (from instanceof SqlIdentifier) {
+            String tableName = ((SqlIdentifier) from).names.get(0);
+            String username = System.getProperty(SECURITY_USERNAME);
+
+            SqlBasicCall permissions = SecurityContext.getInstance().queryPermissions(username, tableName);
+            if (permissions != null) {
+                if (where == null) {
+                    return permissions;
+                }
+                SqlBinaryOperator sqlBinaryOperator = new SqlBinaryOperator(SqlKind.AND.name()
+                        , SqlKind.AND
+                        , 0
+                        , true
+                        , null
+                        , null
+                        , null
+                );
+                SqlNode[] operands = new SqlNode[2];
+                operands[0] = where;
+                operands[1] = permissions;
+                SqlParserPos sqlParserPos = new SqlParserPos(0, 0);
+                SqlBasicCall newWhere = new SqlBasicCall(sqlBinaryOperator, operands, sqlParserPos);
+                LOG.info("from: {}, newWhere: {}", from, newWhere);
+                return newWhere;
+            }
+        }
+        return where;
+    }
+
+    public SqlOperator getOperator() {
+        return SqlSelectOperator.INSTANCE;
+    }
+
+    @Override
+    public SqlKind getKind() {
+        return SqlKind.SELECT;
+    }
+
+    @Override
+    public List<SqlNode> getOperandList() {
+        return ImmutableNullableList.of(keywordList, selectList, from, where,
+                groupBy, having, windowDecls, orderBy, offset, fetch, hints);
+    }
+
+    @Override
+    public void setOperand(int i, SqlNode operand) {
+        switch (i) {
+            case 0:
+                keywordList = Objects.requireNonNull((SqlNodeList) operand);
+                break;
+            case 1:
+                selectList = (SqlNodeList) operand;
+                break;
+            case 2:
+                from = operand;
+                break;
+            case 3:
+                where = operand;
+                break;
+            case 4:
+                groupBy = (SqlNodeList) operand;
+                break;
+            case 5:
+                having = operand;
+                break;
+            case 6:
+                windowDecls = Objects.requireNonNull((SqlNodeList) operand);
+                break;
+            case 7:
+                orderBy = (SqlNodeList) operand;
+                break;
+            case 8:
+                offset = operand;
+                break;
+            case 9:
+                fetch = operand;
+                break;
+            default:
+                throw new AssertionError(i);
+        }
+    }
+
+    public final boolean isDistinct() {
+        return getModifierNode(SqlSelectKeyword.DISTINCT) != null;
+    }
+
+    public final SqlNode getModifierNode(SqlSelectKeyword modifier) {
+        for (SqlNode keyword : keywordList) {
+            SqlSelectKeyword keyword2 =
+                    ((SqlLiteral) keyword).symbolValue(SqlSelectKeyword.class);
+            if (keyword2 == modifier) {
+                return keyword;
+            }
+        }
+        return null;
+    }
+
+    public final SqlNode getFrom() {
+        return from;
+    }
+
+    public void setFrom(SqlNode from) {
+        this.from = from;
+    }
+
+    public final SqlNodeList getGroup() {
+        return groupBy;
+    }
+
+    public void setGroupBy(SqlNodeList groupBy) {
+        this.groupBy = groupBy;
+    }
+
+    public final SqlNode getHaving() {
+        return having;
+    }
+
+    public void setHaving(SqlNode having) {
+        this.having = having;
+    }
+
+    public final SqlNodeList getSelectList() {
+        return selectList;
+    }
+
+    public void setSelectList(SqlNodeList selectList) {
+        this.selectList = selectList;
+    }
+
+    public final SqlNode getWhere() {
+        return where;
+    }
+
+    public void setWhere(SqlNode whereClause) {
+        this.where = whereClause;
+    }
+
+    @Nonnull
+    public final SqlNodeList getWindowList() {
+        return windowDecls;
+    }
+
+    public final SqlNodeList getOrderList() {
+        return orderBy;
+    }
+
+    public void setOrderBy(SqlNodeList orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public final SqlNode getOffset() {
+        return offset;
+    }
+
+    public void setOffset(SqlNode offset) {
+        this.offset = offset;
+    }
+
+    public final SqlNode getFetch() {
+        return fetch;
+    }
+
+    public void setFetch(SqlNode fetch) {
+        this.fetch = fetch;
+    }
+
+    public void setHints(SqlNodeList hints) {
+        this.hints = hints;
+    }
+
+    public SqlNodeList getHints() {
+        return this.hints;
+    }
+
+    public boolean hasHints() {
+        // The hints may be passed as null explicitly.
+        return this.hints != null && this.hints.size() > 0;
+    }
+
+    @Override
+    public void validate(SqlValidator validator, SqlValidatorScope scope) {
+        validator.validateQuery(this, scope, validator.getUnknownType());
+    }
+
+    // Override SqlCall, to introduce a sub-query frame.
+    @Override
+    public void unparse(SqlWriter writer, int leftPrec, int rightPrec) {
+        if (!writer.inQuery()) {
+            // If this SELECT is the topmost item in a sub-query, introduce a new
+            // frame. (The topmost item in the sub-query might be a UNION or
+            // ORDER. In this case, we don't need a wrapper frame.)
+            final SqlWriter.Frame frame =
+                    writer.startList(SqlWriter.FrameTypeEnum.SUB_QUERY, "(", ")");
+            writer.getDialect().unparseCall(writer, this, 0, 0);
+            writer.endList(frame);
+        } else {
+            writer.getDialect().unparseCall(writer, this, leftPrec, rightPrec);
+        }
+    }
+
+    public boolean hasOrderBy() {
+        return orderBy != null && orderBy.size() != 0;
+    }
+
+    public boolean hasWhere() {
+        return where != null;
+    }
+
+    public boolean isKeywordPresent(SqlSelectKeyword targetKeyWord) {
+        return getModifierNode(targetKeyWord) != null;
+    }
+}
